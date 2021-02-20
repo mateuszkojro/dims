e539eac7a6130727b2bb04a76a65d80ca4b40026
Mateusz Kojro <mateuszkojro@outloook.com>
2021-02-20 19:26:50 +0100

    Working not optimized simplex and how to use it

--- simplex/simplex.py
+++ simplex/simplex.py
@@ -3,59 +3,66 @@ import numpy as np
 
 class Simplex:
 
-    iter = 0
-
     def __init__(self, A, b, c):
-        self.A = np.array(A, dtype='float')
-        self.b = np.array(b, dtype='float')
-        self.c = np.array(c, dtype='float')
+        self.A = np.array(A, dtype='float64')
+        # we could append an identity matrix there
+        self.A = np.hstack((self.A, [[1, 0, 0], [0, 1, 0], [0, 0, 1]]))
+        self.b = np.array(b, dtype='float64')
+        self.c = np.array(c, dtype='float64')
+        self.c = np.hstack((self.c, [[0, 0, 0]]))
 
     def optimize(self):
         min_addr = np.argmin(self.c)
-        size_x = self.A.shape[0]
-        size_y = self.A.shape[1]
+
+        size_y = self.A.shape[0]
         min = (256, 0)  # max val, addr
         for i in range(size_y):
             a_val = self.A[i, min_addr]
-            if a_val == 0:
+            if a_val <= 0:
+                print("helllo")
                 continue
-            cur_ratio = self.b[i] / a_val
+            cur_ratio = self.b[i + 1] / a_val
+            print("i", i, "col", min_addr, "b[i]", self.b[i + 1],
+                  "a_val", a_val, "row", i, "ratio", cur_ratio)
             if cur_ratio < min[0]:
                 min = (cur_ratio, i)
-        pivot_el = self.A[min[1], min_addr]
 
         # now we need to operate on the whole tablou so we need to create it
-        r = min[1] + 1
-        n = min_addr + 1
 
         tab = np.vstack((self.c, self.A))  # create_tableau()
-        tab = np.hstack((tab, [[0, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, 1]]))
         self.b = self.b.reshape((4, 1))
         tab = np.hstack((self.b, tab))
+
+        row = min[1] + 1
+        col = min_addr + 1
+        pivot_el = tab[row, col]
 
         print(tab)
         print()
 
         # normalize the values in pivot row
-        tab[r, :] = tab[r, :] / tab[r, n]
+        tab[row, :] = tab[row, :] / tab[row, col]
 
         # pivot rows of A
         for i in range(4):
-            if i != r:
-                mult = tab[i, n] / tab[r, n]
-                tab[i, :] = tab[i, :] - mult * tab[r, :]
+            if i != row:
+                mult = tab[i, col] / tab[row, col]
+                tab[i, :] = tab[i, :] - mult * tab[row, :]
 
-        # print(tab)
-        # print("Pivot col: ", n)
-        # print("pivot row: ", r)
-        # print(pivot_el)
+        print(tab)
+        print("Pivot col: ", col)
+        print("pivot row: ", row)
+        print(pivot_el)
 
         self.A = tab[1:, 1:]
         self.b = tab[:, 0]
         self.c = tab[0, 1:]
 
-        print(self.A)
-        print(self.b)
-        print(self.c)
-
         return tab
+
+    def simplex(self, depth):
+        result = []
+        while(depth):
+            result = self.optimize()
+            depth -= 1
+        return (result[1:, 0].T)
