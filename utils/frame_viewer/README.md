d6329b788443486854ff28ab10421e022b36a4a7
Mateusz Kojro <mateuszkojro@outloook.com>
2021-02-23 22:53:03 +0100

    Show frame can now take args for dimentions of the frame

--- utils/frame_viewer/README.md
+++ utils/frame_viewer/README.md
@@ -1,6 +1,6 @@
 # Script for showing singular frames in YUV422 and comparing them
 
-1. show `frame1` (file with binary data in YUV422 format 1920x1080px)
+1. show `frame1` (file with binary data in YUV422 format  defaults to 1920x1080px)
 ```bash
 python3 show_frame.py frame1
 ```
@@ -10,3 +10,10 @@ python3 show_frame.py frame1
 python3 show_frame.py frame1 frame2
 ```
 
+1. It could also be used with 2 addicional params if frame has another size
+```bash
+python3 show_frame.py frame 1080 1920
+# or
+python show_frame.py frame1 frame2 1024 720
+
+```
--- utils/frame_viewer/show_frame.py
+++ utils/frame_viewer/show_frame.py
@@ -9,24 +9,25 @@ def read_file(path):
     return list(data)
 
 
-def get_img(img):
+def get_img(img, resolution):
 
     img = np.array(img[0::2])
-    img = img.reshape((1080, 1920))
+    img = img.reshape(resolution)
+    # (1080, 1920)
     return img
 
 
-def show_img(path):
-    img = get_img(read_file(path))
+def show_img(path, resolution):
+    img = get_img(read_file(path), resolution)
 
     plt.tight_layout(pad=-1)
     plt.imshow(img)
     plt.show()
 
 
-def show_diff(path1, path2):
-    img1 = get_img(read_file(path2))
-    img2 = get_img(read_file(path2))
+def show_diff(path1, path2, resolution):
+    img1 = get_img(read_file(path2), resolution)
+    img2 = get_img(read_file(path2), resolution)
 
     result = img2 - img1
     result = np.absolute(result)
@@ -47,10 +48,17 @@ def show_diff(path1, path2):
 
 if __name__ == '__main__':
 
-    if len(sys.argv) == 2:
-        show_img(sys.argv[1])
-    elif len(sys.argv) == 3:
-        show_diff(sys.argv[1], sys.argv[2])
+    args = sys.argv
+
+    resolution = (1080, 1920)
+    if len(args) == 2:
+        show_img(args[1], resolution)
+    elif len(args) == 4:
+        show_img(sys.argv[1], (int(args[2]), int(args[3])))
+    elif len(args) == 3:
+        show_diff(args[1], args[2], resolution)
+    elif len(args) == 5:
+        show_diff(args[1], args[2], (int(args[3]), int(args[4])))
     else:
         print("If you want to look at a single frame give path to it \n"
               "if you want compare 2 images give to paths ")
