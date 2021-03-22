b0657a45a318fe3cc3600ba8efbc3a8e0f2c3307
Mateusz Kojro <mateuszkojro@outlook.com>
2021-03-22 18:54:18 +0100

    adding eigen

---
+++ video_analyzer/FrameReader.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mateusz on 3/8/21.
+//
+
+#include "FrameReader.h"
---
+++ video_analyzer/FrameReader.h
@@ -0,0 +1,14 @@
+//
+// Created by mateusz on 3/8/21.
+//
+
+#ifndef VIDEO_ANALYZER_FRAMEREADER_H
+#define VIDEO_ANALYZER_FRAMEREADER_H
+
+
+class FrameReader {
+
+};
+
+
+#endif //VIDEO_ANALYZER_FRAMEREADER_H
