f1f4c329aa6974a0392a7591d93d8ad9db7c4bb3
Mateusz Kojro <mateuszkojro@outlook.com>
2021-07-28 22:42:58 +0200

    Extracted algorithm to separate file

---
+++ motion-detect/CustomAlgorithm.py
@@ -0,0 +1,110 @@
+import math
+import cv2
+from dataclasses import dataclass, astuple
+from functools import cache
+
+
+@dataclass(unsafe_hash=True)
+class Coord:
+    x: int
+    y: int
+
+    @cache
+    def tuple(self):
+        return astuple(self)
+
+
+@dataclass(frozen=True)
+class EventInfo:
+    frame_no: int
+    position: Coord
+    size: Coord
+
+    @cache
+    def center(self):
+        x = self.position.x + (self.size.x / 2)
+        y = self.position.y + (self.size.y / 2)
+        return Coord(int(x), int(y))
+
+    def is_simillar_to(self, event, treshold: float):
+        # check the time between events
+        if abs(self.frame_no - event.frame_no) > 5:
+            return False
+        # and how far apart they are
+        if euc_distance(self.center(), event.center()) < treshold:
+            return True
+        return False
+
+
+class Event:
+    positions: [EventInfo]
+    last_changed: int = 0
+
+    def __init__(self, position: EventInfo):
+        self.positions = []
+        self.positions.append(position)
+        self.last_changed = position.frame_no
+
+    # TODO: This should be done using that
+    #  https://docs.opencv.org/3.1.0/dd/d49/tutorial_py_contour_features.html
+    @cache
+    def bounding_rect(self) -> (int, int):
+        min_x, min_y = self.positions[0].center()
+        max_x, max_y = self.positions[0].center()
+        for item in self.positions:
+            event = item.center()
+            if event.x < min_x:
+                min_x = event.x
+            if event.y < min_y:
+                min_y = event.y
+            if event.x > max_x:
+                max_x = event.x
+            if event.y > max_y:
+                max_y = event.y
+        minimal = math.floor(min_x), math.floor(min_y)
+        maximal = math.floor(max_x), math.floor(max_y)
+        return minimal, maximal
+
+    def path(self) -> (Coord, Coord):
+        start = self.positions[0].position
+        end = self.positions[-1].position
+        return start, end
+
+    def add_if_similar(self, position: EventInfo) -> bool:
+        for item in self.positions:
+            if item.is_simillar_to(position, 15):
+                self.positions.append(position)
+                self.last_changed = position.frame_no
+                return True
+        return False
+
+
+@cache
+def euc_distance(pos1: Coord, pos2: Coord) -> float:
+    return math.sqrt((pos1.x - pos2.x) ** 2 + (pos1.y - pos2.y) ** 2)
+
+
+# Adapted form:
+# https://www.andrewnoske.com/wiki/Code_-_heatmaps_and_color_gradients
+def heatmap_color(val, max_val):
+    val = val / max_val
+    NUM_COLORS = 2
+    color = [[0, 0, 1], [0, 1, 0], [1, 0, 0]]
+    idx1 = None
+    idx2 = None
+    fractBetween = 0
+    if val <= 0:
+        idx1 = idx2 = 0  # ;} // accounts for an input <= 0
+    elif val >= 1:
+        idx1 = idx2 = NUM_COLORS  # accounts for an input >= 0
+    else:
+        val = val * NUM_COLORS
+        idx1 = math.floor(val)
+        fractBetween = val - float(idx1)
+        idx2 = idx1 + 1
+
+    red = (color[idx2][0] - color[idx1][0]) * fractBetween + color[idx1][0]
+    green = (color[idx2][1] - color[idx1][1]) * fractBetween + color[idx1][1]
+    blue = (color[idx2][2] - color[idx1][2]) * fractBetween + color[idx1][2]
+
+    return blue * 255, green * 255, red * 255
--- motion-detect/main.py
+++ motion-detect/main.py
@@ -1,23 +1,22 @@
-# I: Make a convec hull (https://docs.opencv.org/master/dd/d49/tutorial_py_contour_features.html)
-
-# Q: do they alwas go in the straight line
-
-import argparse
-import math
-import time
+#! /usr/bin/python3
 
 import cv2
+import time
 import imutils
 
-SHOW_RAW = False
+from CustomAlgorithm import *
+
+INPUT_PATH = "./file.avi"
+SHOW_RAW = True
 SHOW_DELTA = True
 
 DRAW_BOX = False
 DRAW_PATH = True
 DRAW_STATS = True
 DRAW_CONFIDENCE = True
+HEATMAP = True
 
-TARGET_WIDTH = 900
+TARGET_WIDTH = 1920
 
 WITH_GAUSS = True
 GAUSS_SIGMA = (3, 3)
@@ -25,129 +24,20 @@ GAUSS_SIGMA = (3, 3)
 TRIGER_AREA_TRESHOLD = 5
 DROP_INACTIVE_TIME = 5
 
-
-def calculate_center(position: (int, int), size: (int, int)) -> (float, float):
-    x = position[0] + (size[0] / 2)
-    y = position[1] + (size[1] / 2)
-    return x, y
+EVENT_TRESHOLD = 10
 
 
-def euc_distance(pos1: (float, float), pos2: (float, float)) -> float:
-    return math.sqrt((pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2)
-
-
-class EventInfo:
-    frame_no: int
-    position: (int, int)
-    size: (int, int)
-    center: (float, float)
-
-    def __init__(self, frame_number: int, event_position: (int, int), event_size: (int, int)):
-        self.frame_no = frame_number
-        self.position = event_position
-        self.size = event_size
-        self.center = calculate_center(event_position, event_size)
-
-
-def are_similar(pos1: EventInfo, pos2: EventInfo, treshold: float) -> bool:
-    # check the time between events
-    if abs(pos1.frame_no - pos2.frame_no) > 5:
-        return False
-    # and how far apart they are
-    if euc_distance(pos1.center, pos2.center) < treshold:
-        return True
-    return False
-
-
-class Event:
-    positions: [EventInfo]
-    last_changed: int = 0
-
-    def __init__(self, position: EventInfo):
-        self.positions = []
-        self.positions.append(position)
-        self.last_changed = position.frame_no
-
-    def bounding_rect(self) -> (int, int):
-        min_x, min_y = self.positions[0].center
-        max_x, max_y = self.positions[0].center
-        for item in self.positions:
-            event = item.center
-            if event[0] < min_x:
-                min_x = event[0]
-            if event[1] < min_y:
-                min_y = event[1]
-            if event[0] > max_x:
-                max_x = event[0]
-            if event[1] > max_y:
-                max_y = event[1]
-        minimal = math.floor(min_x), math.floor(min_y)
-        maximal = math.floor(max_x), math.floor(max_y)
-        return minimal, maximal
-
-    def path(self) -> (int, int):
-        start = self.positions[0].position
-        end = self.positions[-1].position
-        return start, end
-
-    def add_if_similar(self, position: EventInfo) -> bool:
-        for item in self.positions:
-            if are_similar(item, position, 15):
-                self.positions.append(position)
-                self.last_changed = position.frame_no
-                return True
-        return False
-
-
-def init_argparse():
-    parser = argparse.ArgumentParser(
-        usage="Give the input file to be analyzed",
-        description="Program detects events on the video recordings of the night sky"
-    )
-    # parser.add_argument("input_file", metavar="input_file",
-    #                     type=str, help="File to be analyzed")
-    # parser.add_argument("draw_path", metavar="draw_path", type=bool, help="Should draw a path of the event",
-    #                     default=DRAW_PATH, required=False)
-    # parser.add_argument("draw_stats", metavar="draw_stats", type=bool, help="Should display performance statistics",
-    #                     default=DRAW_STATS, required=False)
-    # parser.add_argument("draw_confidence", metavar="draw_confidence", type=bool,
-    #                     help="Should display a confidenece of an event", default=DRAW_CONFIDENCE, required=False)
-    #
-    # parser.add_argument("show_raw", metavar="show_raw", type=bool,
-    #                     help="Show raw input frames", default=SHOW_RAW, required=False)
-    #
-    # parser.add_argument("show_delta", metavar="show_delta", type=bool,
-    #                     help="Show the diference between frames and status quo", default=DRAW_CONFIDENCE, required=False)
-    # parser.add_argument("--draw_path", action="store_true", help="Should the path of the event be drawn")
-    return parser
-
-
-if __name__ == '__main__':
-    arg_parser = init_argparse()
-    input_path = "./file.avi"
-    # args = arg_parser.parse_args()
-    #
-    # input_path = args.input_file
-    #
-    # SHOW_RAW = args.show_raw
-    # SHOW_DELTA = args.show_delta
-    #
-    # DRAW_BOX = args.draw_box
-    # DRAW_PATH = args.draw_path
-    # DRAW_STATS = args.draw_stats
-    # DRAW_CONFIDENCE = args.draw_confidence
-
+def main():
     events: [Event] = []
     frame_no = 0
     time_sum = 0
     times = []
-    video_capture = cv2.VideoCapture(input_path)
+    video_capture = cv2.VideoCapture(INPUT_PATH)
 
     frame = None
     first_frame = None
     running = True
     wait_time = 0
-
     while True:
         time.sleep(wait_time)
         start_time = time.monotonic()
@@ -201,8 +91,8 @@ if __name__ == '__main__':
                 continue
             # compute the bounding box for the contour
             (x, y, w, h) = cv2.boundingRect(c)
-            position = (x, y)
-            size = (w, h)
+            position = Coord(x, y)
+            size = Coord(w, h)
             was_added = False
             new_event = EventInfo(frame_no, position, size)
             for event in events:
@@ -221,14 +111,15 @@ if __name__ == '__main__':
             idx += 1
             if DRAW_PATH:
                 rect = event.path()
-                cv2.line(frame, rect[0], rect[1], (0, 255, 0), 3)
+                color = heatmap_color(len(event.positions), EVENT_TRESHOLD) if HEATMAP else (0, 255, 0)
+                cv2.line(frame, rect[0].tuple(), rect[1].tuple(), color, 3)
 
             if DRAW_BOX:
                 rect = event.bounding_rect()
-                cv2.rectangle(frame, rect[0], rect[1], (0, 255, 0), 2)
+                cv2.rectangle(frame, rect[0].tuple(), rect[1].tuple(), (0, 255, 0), 2)
 
             if DRAW_CONFIDENCE:
-                cv2.putText(frame, f"Trigger count: {len(event.positions)}", rect[0], cv2.FONT_HERSHEY_SIMPLEX,
+                cv2.putText(frame, f"Trigger count: {len(event.positions)}", rect[0].tuple(), cv2.FONT_HERSHEY_SIMPLEX,
                             0.35, (0, 0, 255), 1)
 
         end_time = time.monotonic()
@@ -262,3 +153,7 @@ if __name__ == '__main__':
             wait_time = wait_time + .02 if wait_time < 10 else 10
         if key == ord('r'):
             wait_time = 0
+
+
+if __name__ == '__main__':
+    main()
