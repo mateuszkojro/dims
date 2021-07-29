e27eabe4e016ae6abb16a158307e01a5500d9b7a
Mateusz Kojro <mateuszkojro@outlook.com>
2021-07-29 21:23:46 +0200

    preparation for creating dataset

---
+++ motion-detect/FileCrawler.py
@@ -0,0 +1,25 @@
+import os
+
+
+def crawl(path, function, extension=".avi"):
+    result = []
+    size = len(os.listdir(path))
+    for i, obj in enumerate(os.listdir(path)):
+
+        abs_path = os.path.abspath(path + "/" + obj)
+        if os.path.isdir(abs_path):
+            print("Hello there")
+            result += crawl(abs_path, function)
+        else:
+            print(f"Analyzing file {i}/{size} ({i/size * 100}%)")
+            ext = os.path.splitext(abs_path)[-1]
+            if ext == extension:
+                result.append(function(abs_path))
+        if i == 15:
+            break
+    return result
+
+
+if __name__ == '__main__':
+    f = lambda file: print(file)
+    crawl("test", f)
