b781b34821175684eef4181d5264d846178663eb
Mateusz Kojro <mateuszkojro@outlook.com>
2021-08-11 18:05:12 +0200

    Fix typo

--- motion-detect/setup.py
+++ motion-detect/setup.py
@@ -4,11 +4,8 @@ from Cython.Build import cythonize
 setup(
     name='algo',
     ext_modules=cythonize(["CustomAlgorithm.pyx", "CreateDataset.py"],
-<<<<<<< HEAD
                           annotate=True,
-=======
                           # annotate=True,
->>>>>>> origin/master
                           language_level="3"),
     zip_safe=False,
 )
