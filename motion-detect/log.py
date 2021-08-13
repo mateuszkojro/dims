a4d01784e3459dc48e52a85f8e246df8bcd191d0
Mateusz Kojro <mateuszkojro@outlook.com>
2021-08-13 12:07:26 +0200

    Log to stderr (they will be buffered)

--- motion-detect/log.py
+++ motion-detect/log.py
@@ -1,17 +1,20 @@
+from sys import stderr
+
+
 def color(color):
     return lambda string: color + string + "\u001b[0m"
 
 
 def err(msg):
     yellow = color("\u001b[33m")
-    print(yellow("ERR:\t") + msg)
+    print(yellow("ERR:\t") + msg, file=stderr)
 
 
 def critical(msg):
     red = color("\u001b[31m")
-    print(red("CRITICAL:\t") + msg)
+    print(red("CRITICAL:\t") + msg, file=stderr)
 
 
 def info(msg):
     blue = color("\u001b[34;1m")
-    print(blue("INFO:\t") + msg)
+    print(blue("INFO:\t") + msg, file=stderr)
