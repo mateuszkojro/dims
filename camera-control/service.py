32055f6d364f90cfd8af6c4a4f1f85b5551bb316
Mateusz Kojro <mateuszkojro@outlook.com>
2021-08-18 12:53:27 +0200

    Fixing some bugs

--- camera-control/service.py
+++ camera-control/service.py
@@ -6,29 +6,31 @@ import serial
 
 import CameraControll as cc
 
-
-
 FILE_NAME = None
 
 SERIAL_DEVICE = 'COM9'
 
 
+def get_timestamp():
+    return time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
+
+
 def get_camera_status(serial_connection):
     status = []
 
-    status += cc.reqGainStatus(serial_connection)
-    status += cc.reqIrisPosition(serial_connection)
-    status += cc.reqShutterStatus(serial_connection)
-    status += cc.reqFanStatus(serial_connection)
-    status += cc.reqTemperatureStatus(serial_connection)
-
+    status.append(get_timestamp())
+    status.append(cc.reqGainStatus(serial_connection))
+    status.append(cc.reqIrisPosition(serial_connection))
+    status.append(cc.reqShutterStatus(serial_connection))
+    status.append(cc.reqFanStatus(serial_connection))
+    status.append(cc.reqTemperatureStatus(serial_connection))
     return status
 
 
 def get_ufo_captrue_status():
 
     raise NotImplementedError
-    
+
     # coding: utf-8
 
     # pip install pillow pywinauto
@@ -57,12 +59,17 @@ def get_ufo_captrue_status():
 
 def get_serial_connection():
     #_devName = '/dev/tty.usbserial-FTS4PT0F'
-    serial_connection = serial.Serial(port=SERIAL_DEVICE,
-                                      baudrate=9600,
-                                      parity=serial.PARITY_NONE,
-                                      bytesize=serial.EIGHTBITS,
-                                      stopbits=serial.STOPBITS_ONE,
-                                      timeout=1)
+    try:
+        serial_connection = serial.Serial(port=SERIAL_DEVICE,
+                                          baudrate=9600,
+                                          parity=serial.PARITY_NONE,
+                                          bytesize=serial.EIGHTBITS,
+                                          stopbits=serial.STOPBITS_ONE,
+                                          timeout=1)
+    except Exception as e:
+        print(f"Connection to serial port {SERIAL_DEVICE} "
+              "failed with: {e} running in debug mode")
+        serial_connection = None
     return serial_connection
 
 
@@ -70,7 +77,8 @@ def save_log():
 
     serial_connecton = get_serial_connection()
     data_point = get_camera_status(serial_connecton)
-    serial_connecton.close()
+
+    if serial_connecton: serial_connecton.close()
 
     print(f"Saving current status: {data_point}")
 
@@ -85,15 +93,15 @@ def save_log():
 
 if __name__ == '__main__':
 
-    FILE_NAME = f'{time.strftime("%y-%m-%d_%H:%M:%S", time.localtime())}.csv'
+    FILE_NAME = get_timestamp() + '.csv'
 
     with open(FILE_NAME, 'w') as outputfile:
 
-        outputfile.write("Hello")
-        # Write title columns
-        pass
+        outputfile.write(
+            "Timestamp, Gain, Iris position, Shutter, Fan status, Temperature\n"
+        )
 
-    schedule.every(30).minutes.do(save_log)
+    schedule.every(0.5).minutes.do(save_log)
 
     while True:
         schedule.run_pending()
