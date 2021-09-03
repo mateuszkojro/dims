4f64ffe833f8b6b3753016f041d4cba35893e6ab
Mateusz Kojro <mateuszkojro@outlook.com>
2021-09-03 11:14:46 +0200

    Cleanup

--- labeling/parse_annotations.py
+++ labeling/parse_annotations.py
@@ -14,21 +14,21 @@ def read_label_studio_output(file_path):
 
         data = json.loads(content)
 
-        lables_for_events = np.array([])
+        lables_for_events = np.array([], dtype=object)
 
         for row in tqdm(data):
-            print(f"=== NEW FILE ===")
-            labels = np.array([])
+            labels = np.array([], dtype=object)
             for annotation in row["annotations"]:
                 for result in annotation["result"]:
                     for label in result["value"]["choices"]:
                         labels = np.append(labels, label)
             filename = row["data"]["image"]
-            lables_for_events = np.append(lables_for_events, (filename, labels))
-        
+            lables_for_events = np.append(lables_for_events,
+                                          (filename, labels))
+
         return lables_for_events
 
 
 if __name__ == '__main__':
     labels = read_label_studio_output(sys.argv[1])
-    print(labels)
+    print(f"Parsed: {len(labels)} datapoints")
