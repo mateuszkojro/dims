b26ccf0c6d871b19544c76ec6dfb00f5019f8f76
Mateusz Kojro <mateuszkojro@outlook.com>
2021-09-03 11:08:03 +0200

    Refactor labels annotations

--- labeling/parse_annotations.py
+++ labeling/parse_annotations.py
@@ -1,23 +1,34 @@
 import json
+import sys
+import numpy as np
+
 try:
-    from tqdm import *
+    from tqdm import tqdm
 except Exception:
     tqdm = lambda x: x
 
-import time
 
-if __name__ == '__main__':
-
-    content = None
-    with open("annotations.json") as f:
+def read_label_studio_output(file_path):
+    with open(file_path) as f:
         content = f.read()
 
-    data = json.loads(content)
+        data = json.loads(content)
 
-    for row in tqdm(data):
-        filename = row["data"]["image"]
-        print(f"=== NEW FILE ===")
-        for annotation in row["annotations"]:
-            for result in annotation["result"]:
-                for label in result["value"]["choices"]:
-                    print(f"{label=}")
+        lables_for_events = np.array([])
+
+        for row in tqdm(data):
+            print(f"=== NEW FILE ===")
+            labels = np.array([])
+            for annotation in row["annotations"]:
+                for result in annotation["result"]:
+                    for label in result["value"]["choices"]:
+                        labels = np.append(labels, label)
+            filename = row["data"]["image"]
+            lables_for_events = np.append(lables_for_events, (filename, labels))
+        
+        return lables_for_events
+
+
+if __name__ == '__main__':
+    labels = read_label_studio_output(sys.argv[1])
+    print(labels)
